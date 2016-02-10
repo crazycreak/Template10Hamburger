18ac400a54dc231e192b5124acc9096c9f701327
christoph <christoph@felberbauer.com>
2016-02-10 19:41:26 +0100

    initial Template10Hamburger Project commit

---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+using Template10Hamburger.ViewModels;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Template10Hamburger.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Enabled;
+        }
+    }
+}
